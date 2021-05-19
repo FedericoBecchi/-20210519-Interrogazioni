db02e9d37e10bf76b5e0bae347e3195fc1baf931
FedericoBecchi <sfederico.becchi@itis.pr.it>
2021-05-19 13:09:19 +0200

    Interrogazioni
    Classe Interrogazioni
    La quale contiene lo scheletro del programma

---
+++ src/Interrogazioni.java
@@ -0,0 +1,45 @@
+public class Interrogazioni {
+	
+	/** Numero di persone della lista**/
+	private int n_persone;
+	
+	/** Lista Utenti  **/
+	// private ArrayList<Studenti> Studenti;
+	
+	/** Lista Utenti  **/
+	// private ArrayList<Studenti> Studenti_Sorteggiati;
+	
+	public Interrogazioni(int n_interrogati) {
+		this.n_persone = n_interrogati;
+	}
+	
+	/** Questo Metodo Genera La Lista Di Sorteggiati **/
+	public void Sorteggia() {
+		// Usare Il Metodo Random 
+		// Poi Togliere L'utente se è gia stato Inserito
+			// CREARE UNA COPIA DI ARRAYLIST UTENTI;
+			// INSERIRLO NELLA DATA GIUSTA
+	}
+	
+
+	/** Questo Metodo Stampa Gli Utenti **/
+	public void StampaUtenti() {
+		
+	}
+	
+	/** Questo Metodo Gestisce Le Date **/
+	public void GestisciData() {
+		// Come Posso Gestire Le Date
+		// Ipotesi 
+		// Gestire Con ArrayListi di  String
+			// Creare Attributo ArrayList String
+			// Esiste La Classe Data per ottenere la data corrente
+			// Creare Attributo Int Giorni Di Interrogazionu
+			// Creare Attributi Per I Sostituti
+	}
+	
+	
+	
+	
+	
+}
