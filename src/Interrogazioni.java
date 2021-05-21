fa469853fb8299f7c10416cf0eba8ab6c7a3b6c7
FedericoBecchi <sfederico.becchi@itis.pr.it>
2021-05-21 14:10:57 +0200

    Revert "Revert "Interrogazioni 6.0""
    This reverts commit c6338826a5ae2cef980339db719c713d71fa8384.

--- src/Interrogazioni.java
+++ src/Interrogazioni.java
@@ -12,6 +12,9 @@ public class Interrogazioni {
 	
 	/** Nummero di sessioni **/
 	private int n_sessioni;
+	
+	/** Stringa del ritorno**/
+	private String ritorno;
 
 	/** Lista Utenti  **/
 	 private ArrayList<Studente> studenti;
@@ -22,10 +25,12 @@ public class Interrogazioni {
 	 /** Contiene la lista di date **/
 	 private ArrayList<String> date_sessione;
 	
-	public Interrogazioni(int n_interrogati,int n_studenti_sessioni,int n_sessioni) {
-		this.n_persone = n_interrogati;
-		this.n_sessioni = n_sessioni;
-		this.n_studenti_sessione = n_studenti_sessioni;
+	public Interrogazioni(int n_persone,int n_studenti_sessione,int n_sessioni) {
+		if(n_persone>0) {this.n_persone = n_persone;}
+		if(n_studenti_sessione>0) {this.n_studenti_sessione = n_studenti_sessione;}
+		if(n_sessioni>0) {this.n_sessioni = n_sessioni;}
+		
+		this.ritorno = "";
 		this.studenti = new ArrayList<Studente>();
 		this.studenti_sorteggiati = new ArrayList<Studente>();
 		this.date_sessione = new ArrayList<String>();
@@ -53,6 +58,14 @@ public class Interrogazioni {
 
 	public ArrayList<Studente> getStudenti() {
 		return studenti;
+	}
+	
+	public String getRitorno() {
+		return ritorno;
+	}
+
+	public void setRitorno(String ritorno) {
+		this.ritorno = ritorno;
 	}
 	
 	/** Restuisce True Se Lo Studente è Contenuto Nell ArrayList**/
@@ -128,30 +141,32 @@ public class Interrogazioni {
 	/** Questo Metodo Genera La Lista Di Sorteggiati **/
 	public void Sorteggio() {
 		ArrayList<Studente> copia_studenti = this.studenti;
-		this.studenti_sorteggiati = new ArrayList<Studente>();
 		Random rand = new Random();
 		int sorteggiato;
-		for(int i  = copia_studenti.size()-1;i>=0;i--) {
-			sorteggiato = rand.nextInt((i - 0) + 1) + 0;
-			this.studenti_sorteggiati.add(copia_studenti.get(sorteggiato));
-			copia_studenti.remove(sorteggiato);
+		for(int j = 0;j<this.n_sessioni;j++) {
+			for(int i  = copia_studenti.size()-1;i>=0;i--) {
+				sorteggiato = rand.nextInt((i - 0) + 1) + 0;
+				this.studenti_sorteggiati.add(copia_studenti.get(sorteggiato));
+				copia_studenti.remove(sorteggiato);
+			}
 		}
 	}
 	
 	/** Questo Metodo Crea Una Stringa con gli utenti sorteggiati **/
-	public String ArchiviaUtenti() {
-		String ritorno = "";
-		/** Valore Per Stabilire il giusto studente da stampare **/
+	public void ArchiviaUtenti() {
 		int j = 0;
-		for(int i = 0;i<this.n_sessioni;i++) {
-			ritorno += this.getDataIndex(i);
-			for(int k = j ;k<=this.n_studenti_sessione;k++) {
-				ritorno += "\n"+this.getStudenteSorteggiatoIndex(k).getCognome();
-				j = k;
+		/** Valore Per Stabilire il giusto studente da stampare **/
+		if(this.n_sessioni==this.date_sessione.size()&&this.n_persone==this.studenti_sorteggiati.size()&&this.n_persone>0) {
+			for(int i = 0;i<this.date_sessione.size();i++) {
+				this.ritorno += "\n"+this.getDataIndex(i);
+				for(int k = j;k<this.n_studenti_sessione+(this.n_studenti_sessione*i);k++) {
+					ritorno+= "\n"+this.studenti_sorteggiati.get(k).getCognome();
+					j++;
+				}
+				this.ritorno += "\n";
+				if(j==this.studenti_sorteggiati.size()) {break;}
 			}
-			ritorno += "\n";
 		}
-		return ritorno;
 	}
 	
 	/** Questo Metodo Gestisce Le Date **/
@@ -166,18 +181,28 @@ public class Interrogazioni {
 //  }
 	
 	public static void main(String[] args) {
-		Interrogazioni prova = new Interrogazioni(5,4,1);
+		
+		
+		
+		int n_interrogati = 3;
+		int n_studenti_sessione = 3;
+		int n_sessioni = 0;
+		
+		Interrogazioni prova = new Interrogazioni(n_interrogati,n_studenti_sessione,n_sessioni);
 		prova.addStudente(new Studente("Ferioli","Andrea","Inglese","4AINF"));
 		prova.addStudente(new Studente("Becchi","Federico","Inglese","4AINF"));
 		prova.addStudente(new Studente("Zhu","Massimo","Inglese","4AINF"));
 		prova.addStudente(new Studente("Mazza","GianMarco","Inglese","4AINF"));
 		prova.addStudente(new Studente("Perazzoli","Davide","Inglese","4AINF"));
+		prova.addStudente(new Studente("Calabrese","Alessandro","Inglese","4AINF"));
 		
 		prova.addData("3 Maggio");
 		prova.addData("4 Maggio");
 		
 		prova.Sorteggio();
-		System.out.println(prova.ArchiviaUtenti());
+		prova.ArchiviaUtenti();
+		
+		System.out.println(prova.getRitorno());
 	}
 	
 }
